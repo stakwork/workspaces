489d8e9ab19c69a2a34ad3ba5e9ceb71fdd94f5e
Gonzalo Javier Aune <gonzaloaune@gmail.com>
2025-05-29 19:08:47 +0100

    deployment for workspace

---
+++ kubernetes/workspace_controller/deploy-workspace.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+# Load environment variables
+if [[ -f ../../.env ]]; then
+  set -a
+  source ../../.env
+  set +a
+else
+  echo "❌ .env file not found!"
+  exit 1
+fi
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+
+docker buildx build --platform linux/amd64 -t $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/workspace-controller:latest --push .
+
+TAG=$(date +%Y%m%d%H%M%S)
+
+docker tag $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/workspace-controller:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/workspace-controller:$TAG
+
+docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/workspace-controller:$TAG
+
+echo $TAG
+
