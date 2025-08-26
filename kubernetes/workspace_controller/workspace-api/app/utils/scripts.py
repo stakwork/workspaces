566f93cd00817bd0910f3a02dcb2767b737eea34
Gonzalo Javier Aune <gonzaloaune@gmail.com>
2025-08-26 10:02:41 +0100

    removing conflicting moby-tiny package

--- kubernetes/workspace_controller/workspace-api/app/utils/scripts.py
+++ kubernetes/workspace_controller/workspace-api/app/utils/scripts.py
@@ -133,6 +133,8 @@ def create_post_start_command():
                 elif [ "$OS" = "ubuntu" ]; then
                     # Ubuntu-specific Docker installation
                     echo "Setting up Docker for Ubuntu $VERSION_CODENAME"
+
+                    apt remove --purge containerd.io moby-tini
                     
                     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
                     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $VERSION_CODENAME stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
